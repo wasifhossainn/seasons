2f7c0437e9b6374e7efbf9760a1c5d987477f241
wasif <wasif.hossain@hotmail.com>
2019-11-26 23:38:23 +1100

    SEASON DETECTION ADDED

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,13 +1,39 @@
-import React from 'react'
+import React from 'react';
+import './SeasonDisplay.css';
 
+//Configuration Object created which has 2 key values pairs inside
+const seasonConfig = {
+	//Summer object
+	summer: {
+		text: "Let's hit the beach",
+		iconName: 'sun'
+	},
 
-const SeasonDisplay = () => {
-    return (
-        <div>
-            Season Display
-        </div>
-    )
-}
+	//Winter object
+	winter: {
+		text: 'Brrr its cold',
+		iconName: 'snowflake'
+	}
+};
 
+const getSeason = (lat, month) => {
+	if (month > 2 && month < 9) {
+		return lat > 0 ? 'summer' : 'winter';
+	} else {
+		return lat > 0 ? 'winter' : 'summer';
+	}
+};
 
-export default SeasonDisplay
+const SeasonDisplay = (props) => {
+	const season = getSeason(props.lat, new Date().getMonth);
+	const { text, iconName } = seasonConfig[season];
+	return (
+		<div className={`season-display ${season}`}>
+			<i className={`icon-left massive ${iconName} icon`} />
+			<h1>{text}</h1>
+			<i className={`icon-right massive ${iconName} icon`} />
+		</div>
+	);
+};
+
+export default SeasonDisplay;
--- src/index.js
+++ src/index.js
@@ -1,50 +1,26 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import SeasonDisplay from './SeasonDisplay'
-
-
-
+import React from 'react';
+import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
 
 class App extends React.Component {
-    constructor(props) {
-      super(props)
+	state = { lat: null, errorMessage: '' };
 
-      this.state = {
-          lat: null,
-          errorMessage: ''
-      }
+	componentDidMount() {
+		window.navigator.geolocation.getCurrentPosition(
+			(position) => this.setState({ lat: position.coords.latitude }),
+			(err) => this.setState({ errorMessage: err.message })
+		);
+	}
 
-      window.navigator.geolocation.getCurrentPosition(
-        (position) => {
-            //Update our state we use setState
-            this.setState({
-                lat:position.coords.latitude 
-            })
-        },
-        (err) => 
-        this.setState({
-            errorMessage: err.message
-           
-        })
-        
-
-    )
-    }
-
-
-
-    render() {
-        if (this.state.errorMessage && !this.state.lat) {
-            return <div> Error: {this.state.errorMessage} </div>
-        }
-        if (!this.state.errorMessage && this.state.lat) {
-            return <div> Latitude: {this.state.lat} </div>
-        }
-        return <div> Loading... </div>
-        
-
-        }
+	render() {
+		if (this.state.errorMessage && !this.state.lat) {
+			return <div> Error: {this.state.errorMessage} </div>;
+		}
+		if (!this.state.errorMessage && this.state.lat) {
+			return <SeasonDisplay lat={this.state.lat} />;
+		}
+		return <div> Loading... </div>;
+	}
 }
 
-
-ReactDOM.render(<App/>, document.querySelector('#root'))
+ReactDOM.render(<App />, document.querySelector('#root'));
